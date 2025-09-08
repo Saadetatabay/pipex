2870ac72c017e227037831d3436ff37dfbd62ba8
saadetatabay <etatabaysaad143@gmail.com>
2025-09-08 19:50:35 +0300

    fork ile execlp

--- deneme6.c
+++ deneme6.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include<unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 int main() {
-    printf("Benim programım başladı!\n");
+    // printf("Benim programım başladı!\n");
 
-    // "ls -l" komutunu çalıştır
-    execlp("ping","ping","-c","3","google.com",NULL);
-    //execlp("ls", "ls", "-l", NULL);
+    // // "ls -l" komutunu çalıştır
+    // execlp("ping","ping","-c","3","google.com",NULL);
+    // //execlp("ls", "ls", "-l", NULL);
 
-    // Buraya GELEMEZ çünkü exec başarılı olursa geri dönmez
-    printf("Bunu asla göremezsin!\n");
+    // // Buraya GELEMEZ çünkü exec başarılı olursa geri dönmez
+    // printf("Bunu asla göremezsin!\n");
 
-    return 0;
+    // return 0;
+    int pid = fork();
+    if (pid == 0)
+    {
+        printf("child process\n");
+        execlp("ls","ls","-l",NULL);
+    }
+    else{
+        printf("parent process,child process working\n");
+        wait(NULL);
+        printf("SUCCESS\n"); 
+    }
 }
